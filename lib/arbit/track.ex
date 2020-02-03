cc9abeadd3050d5d171a0625e5936cc4893cd6be
Pushkar Singh <theagilecoder@gmail.com>
2020-02-03 20:17:36 +0530

    Refactor Track.ex

--- lib/arbit/track.ex
+++ lib/arbit/track.ex
@@ -11,26 +11,8 @@ defmodule Arbit.Track do
   alias Arbit.Track.Bitbns
 
   ############
-  # Uses API #
+  #  Bitbns  #
   ############
-
-  def upsert_conversion do
-    %Currency{}
-    |> struct(%{pair: "USD-INR"})  # Merge map into Currency struct
-    |> struct(%{amount: Currency.fetch_conversion()})
-    |> Repo.insert(on_conflict: {:replace, [:amount, :updated_at]}, conflict_target: :pair)
-  end
-
-  def upsert_coinbase_portfolio do
-    conversion_amount = get_conversion_amount("USD-INR")
-
-    # Merge price_inr in each product in the portfolio
-    # & Parallely upsert each product in coinbase table
-    Coinbase.fetch_portfolio()
-    |> Enum.map(fn %{price_usd: price_usd} = product -> struct!(product, %{price_inr: (price_usd * conversion_amount) |> Float.round(2)}) end)
-    |> Task.async_stream(&Repo.insert(&1, on_conflict: {:replace, [:price_usd, :price_inr, :updated_at]}, conflict_target: :product))
-    |> Enum.map(fn {:ok, result} -> result end)
-  end
 
   def upsert_bitbns_portfolio do
     conversion_amount = get_conversion_amount("USD-INR")
@@ -43,15 +25,45 @@ defmodule Arbit.Track do
     |> Enum.map(fn {:ok, result} -> result end)
   end
 
+  #############
+  #  Coinbase #
+  #############
+
+  def upsert_coinbase_portfolio do
+    conversion_amount = get_conversion_amount("USD-INR")
+
+    # Merge price_inr in each product in the portfolio
+    # & Parallely upsert each product in coinbase table
+    Coinbase.fetch_portfolio()
+    |> Enum.map(fn %{price_usd: price_usd} = product -> struct!(product, %{price_inr: (price_usd * conversion_amount) |> Float.round(2)}) end)
+    |> Task.async_stream(&Repo.insert(&1, on_conflict: {:replace, [:price_usd, :price_inr, :updated_at]}, conflict_target: :product))
+    |> Enum.map(fn {:ok, result} -> result end)
+  end
+
+  def create_coinbase(attrs) do
+    %Coinbase{}
+    |> struct(attrs)  # Merge map into Currency struct
+    |> Repo.insert(on_conflict: {:replace, [:price_usd, :updated_at]}, conflict_target: :product)
+  end
+
   ####################
-  # Uses CLI Currency#
+  #     Currency     #
   ####################
+
+  def upsert_conversion do
+    %Currency{}
+    |> struct(%{pair: "USD-INR"})  # Merge map into Currency struct
+    |> struct(%{amount: Currency.fetch_conversion()})
+    |> Repo.insert(on_conflict: {:replace, [:amount, :updated_at]}, conflict_target: :pair)
+  end
 
   def list_currencies,          do: Repo.all(Currency)
 
   def get_currency!(id),        do: Repo.get!(Currency, id)
 
   def get_currency_by!(params), do: Repo.get_by!(Currency, params)
+
+  def delete_currency(%Currency{} = currency), do: Repo.delete(currency)
 
   def get_conversion_amount(pair) do
     %{amount: amount} = get_currency_by!(%{pair: pair})
@@ -62,20 +74,6 @@ defmodule Arbit.Track do
     %Currency{}
     |> struct(attrs)  # Merge map into Currency struct
     |> Repo.insert(on_conflict: {:replace, [:amount, :updated_at]}, conflict_target: :pair)
-  end
-
-  def delete_currency(%Currency{} = currency) do
-    Repo.delete(currency)
-  end
-
-  ####################
-  # Uses CLI Coinbase#
-  ####################
-
-  def create_coinbase(attrs) do
-    %Coinbase{}
-    |> struct(attrs)  # Merge map into Currency struct
-    |> Repo.insert(on_conflict: {:replace, [:price_usd, :updated_at]}, conflict_target: :product)
   end
 
 end
