0eec48306dd8033f19ae8ea94ba1dadeacca554d
Pushkar Singh <theagilecoder@gmail.com>
2020-02-06 19:58:41 +0530

    Show sorted and correct values in /usatoindia

--- lib/arbit_web/views/result_view.ex
+++ lib/arbit_web/views/result_view.ex
@@ -1,4 +1,11 @@
 defmodule ArbitWeb.ResultView do
   use ArbitWeb, :view
 
+  alias Arbit.Track.Result
+
+  def usatoindia(results) do
+    results
+    |> Enum.filter(fn x -> x.difference > 0 end)
+    |> Enum.sort_by(fn p -> p.difference end, &>=/2)
+  end
 end
