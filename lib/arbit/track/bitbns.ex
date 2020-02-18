08863d299bb84fdf6506218d7e4bd8408a1da978
Pushkar Singh <theagilecoder@gmail.com>
2020-02-18 19:56:13 +0530

    Refactor Bitbns model

--- lib/arbit/track/bitbns.ex
+++ lib/arbit/track/bitbns.ex
@@ -11,29 +11,35 @@ defmodule Arbit.Track.Bitbns do
   end
 
   @doc """
-    Returns API URL of a given product
+  Returns list of %Bitbns{} structs ex. %Bitbns{product: "BTC-INR", price_inr: 1.11}
   """
-  def url do
-    "https://bitbns.com/order/getTickerWithVolume/"
-  end
-
-  @doc """
-    Returns Bitbns's list of products
-  """
-  def product_list do
-    %{body: body} = HTTPoison.get! url()
-    Jason.decode!(body, [keys: :atoms])
-    # |> Map.keys() |> Enum.sort() |> IO.inspect(limit: :infinity, width: 0)
-  end
-
   def fetch_portfolio do
     product_list()
     |> Enum.map(&create_bitbns_struct/1)
   end
 
-  defp create_bitbns_struct({key, %{lowest_sell_bid: lowest_sell_bid}}) do
-    %Bitbns{}
-    |> struct(%{product: to_string(key)<>"-INR"})
-    |> struct(%{price_inr: lowest_sell_bid/1 |> Float.round(2)})
+  defp product_list do
+    %{body: body} = HTTPoison.get! url()
+    Jason.decode!(body, [keys: :atoms])
+    # |> Map.keys() |> Enum.sort() |> IO.inspect(limit: :infinity, width: 0)
+  end
+
+  defp url do
+    "https://bitbns.com/order/getTickerWithVolume/"
+  end
+
+  defp create_bitbns_struct({key, value}) do
+    case value do
+      %{lowest_sell_bid: lowest_sell_bid} ->
+        %Bitbns{}
+        |> struct(%{product: to_string(key)<>"-INR"})
+        |> struct(%{price_inr: lowest_sell_bid/1 |> Float.round(2)})
+
+      _ ->
+        %Bitbns{}
+        |> struct(%{product: to_string(key)<>"-INR"})
+        |> struct(%{price_inr: 0})
+    end
+
   end
 end
